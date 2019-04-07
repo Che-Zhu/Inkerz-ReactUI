0167f96f0fd64c246cbe0873f7ec4cb8c6bd92a2
Che <30403707+Cheaww@users.noreply.github.com>
2019-04-07 17:23:49 +1000

    Updates

---
+++ precache-manifest.49eeadbffb82c8fa984df8bd2e236537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Inkerz-ReactUI/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "75d8658c9a2a19806265",
+    "url": "/Inkerz-ReactUI/static/js/runtime~main.b203575a.js"
+  },
+  {
+    "revision": "b84d201c6f322a1c7923",
+    "url": "/Inkerz-ReactUI/static/js/main.15b6569c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Inkerz-ReactUI/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b84d201c6f322a1c7923",
+    "url": "/Inkerz-ReactUI/static/css/main.d8f1f685.chunk.css"
+  },
+  {
+    "revision": "2636504e9aded26e9d1606d1c5a0740c",
+    "url": "/Inkerz-ReactUI/index.html"
+  }
+];
