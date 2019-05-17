62c69c06a3286e3c6a9b5578051c95e72dd30078
loaysetrak <48985250+loaysetrak@users.noreply.github.com>
2019-05-17 22:29:39 +1000

    Update SearchControl.js

--- src/components/SearchControl.js
+++ src/components/SearchControl.js
@@ -5,14 +5,16 @@ import React from 'react';
 function SeachControl(props) {
     
     return (
-        <div className="search-control">   
+        <div className="search-control"> 
             <input
             id="text-to-search"
              className="search-3d"
             onChange={props.onCaseSearch}
             value={props.searched} 
             placeholder="Search Case..."
-        />        
+            style={{backgroundImage: "url(images/search-icon.png)",
+            backgroundRepeat: 'no-repeat'}}
+        />      
         <button className="clearButton" onClick={() => props.onClear("")}>X</button>
             
         </div>
