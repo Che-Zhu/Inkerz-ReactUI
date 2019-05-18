1da60e0f7bbeae9997f1424a4912d01896d5d44d
Che <30403707+Cheaww@users.noreply.github.com>
2019-05-18 17:37:10 +1000

    Export

--- src/components/ExportControl.js
+++ src/components/ExportControl.js
@@ -2,20 +2,32 @@
 onChange={props.on3DFormatSelect} updates App.js chosen_export_3d_format value*/
 
 import React from 'react';
+import { saveAs } from 'file-saver';
 
-function ExportControl(props) {
-    return (
-        <div className="export-control">
-            <select className="select-3d" onChange={props.on3DFormatSelect}>
-                <option value="">Select Format</option>
-                {props.export_3d_formats.map((format) => (
-                    <option key={format} value={format} >.{format}</option>
-                ))}
-            </select>
-            {/* NEEDS a function to export 3D model bound in App.js!!!! */}
-            <button className="button-3d">Export 3D Model</button>
-        </div>
-    )
+class ExportControl extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    clickHandler() {
+        var FileSaver = require('file-saver');
+        console.log(this.props.app_states.chosen_phone_case)
+        var file = new File([this.props.app_states.chosen_phone_case], "exported.obj", {type: "text/plain;charset=utf-8"});
+        FileSaver.saveAs(file);
+    }
+    render() {
+        return (
+            <div className="export-control">
+                <select className="select-3d" onChange={this.props.on3DFormatSelect}>
+                    <option value="">Select Format</option>
+                    {this.props.export_3d_formats.map((format) => (
+                        <option key={format} value={format} >.{format}</option>
+                    ))}
+                </select>
+                {/* NEEDS a function to export 3D model bound in App.js!!!! */}
+                <button ref="download" className="button-3d" onClick={() => this.clickHandler()}>Export 3D Model</button>
+            </div>
+        )
+    }
 }
 
 export default ExportControl
